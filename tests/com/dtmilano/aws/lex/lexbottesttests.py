2aa9bcba8a2118dd25cd694db79031f164153c5f
Diego Torres Milano <dtmilano@gmail.com>
2017-12-10 02:50:55 -0500

    Verbosity from command line

--- tests/com/dtmilano/aws/lex/lexbottesttests.py
+++ tests/com/dtmilano/aws/lex/lexbottesttests.py
@@ -18,6 +18,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import re
+import sys
 import unittest
 
 from com.dtmilano.aws.lex.conversation import Conversation, ConversationItem
@@ -25,10 +26,10 @@ from com.dtmilano.aws.lex.lexbottest import LexBotTest
 from com.dtmilano.aws.lex.lexmodelsclient import LexModelsClient
 from com.dtmilano.aws.lex.lexruntimeclient import DialogState
 
-VERBOSE = False
-
 RE_DATE = re.compile('\d+-\d+-\d+')
 RE_WEEK = re.compile('\d+-w\d+')
+
+verbose = False
 
 
 class LexBotTestTests(LexBotTest):
@@ -57,7 +58,7 @@ class LexBotTestTests(LexBotTest):
                 conversations.append(Conversation(
                     ConversationItem('Cancel', r(DialogState.READY_FOR_FULFILLMENT))
                 ))
-        self.conversations_text(bot_name, bot_alias, user_id, conversations, VERBOSE)
+        self.conversations_text(bot_name, bot_alias, user_id, conversations, verbose)
 
     def test_conversations_text_book_car(self):
         bot_name = 'BookTrip'
@@ -66,9 +67,9 @@ class LexBotTestTests(LexBotTest):
         conversation_definition = {
             'BookCar': [
                 ('book a car', DialogState.ELICIT_SLOT, {}),
-                ('LA', DialogState.ELICIT_SLOT, {}),
-                ('next week', DialogState.ELICIT_SLOT, {'pick_up_date': RE_WEEK}),
-                ('a month from now', DialogState.ELICIT_SLOT, {'return_date': RE_DATE}),
+                ('L.A.', DialogState.ELICIT_SLOT, {}),
+                ('next week', DialogState.ELICIT_SLOT, {'PickUpDate': RE_WEEK}),
+                ('a month from now', DialogState.ELICIT_SLOT, {'ReturnDate': RE_DATE}),
                 ('25', DialogState.ELICIT_SLOT, {}),
                 ('economy', DialogState.CONFIRM_INTENT, {}),
                 ('yes', DialogState.READY_FOR_FULFILLMENT, {}),
@@ -77,8 +78,9 @@ class LexBotTestTests(LexBotTest):
                 ('cancel', DialogState.READY_FOR_FULFILLMENT, {})
             ]
         }
-        self.conversations_text_helper(bot_alias, bot_name, user_id, conversation_definition, VERBOSE)
+        self.conversations_text_helper(bot_alias, bot_name, user_id, conversation_definition, verbose)
 
 
 if __name__ == '__main__':
+    verbose = '-v' in sys.argv or '--verbose' in sys.argv
     unittest.main()
